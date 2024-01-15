97c57a4a1669a18f113d07dc3c9710e2f14ae875
Undying-1 <bear.lvb@gmail.com>
2024-01-15 19:09:02 +0100

    Fixed: Urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('employees/', include('apps.Employees.urls')),
+    path('employees', include('apps.Employees.urls')),
     path('applications', include('apps.Applications.urls')),
     path('services', include('apps.Services.urls')),
     path('contacts/', include('apps.Contacts.urls')),
