c2f706a4bd644d93a018889a514cb68c82f08708
Undying-1 <bear.lvb@gmail.com>
2024-01-31 17:30:11 +0100

    Modified: Projects.url

--- apps/Projects/urls.py
+++ apps/Projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects', views.ProjectView.as_view()),
+    path('', views.ProjectView.as_view()),
     path('<int:pk>', views.SingleProjectView.as_view()),
 ]
